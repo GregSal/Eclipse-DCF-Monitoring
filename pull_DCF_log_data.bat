5f3d61a0536df48b8765caf0f3a3e73a2be3071b
Greg Salomons <Gregory.Salomons@kingstonhsc.ca>
2023-09-13 09:50:55 -0400

    Exclude local files

--- pull_DCF_log_data.bat
+++
@@ -1,5 +0,0 @@
-CALL C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
-CALL conda activate DCF_Tracking
-Cd "Z:\Treatment Planning System\DCF Monitoring and Configuration"
-Z:
-C:/ProgramData/Anaconda3/envs/DCF_Tracking/python.exe "z:/Treatment Planning System/DCF Monitoring and Configuration/pull_dcf_history.py"
